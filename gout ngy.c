88fdc6a22cfcf055dfb50f78c848b2d4b108e2f1
hoa-dev <vhoa21710@gmail.com>
2020-11-08 17:21:24 +0700

    Create gout ngy.c

---
+++ gout ngy.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**Tieu chuan dau vao **/
+void main ()
+{
+	int chieucao = 158, cannang = 43, mautoc = 'nau', sdbv = 905890 ,y ,n ;
+	char ans [ 6];
+	printf("Nhap vao chieu cao cua ban : ");
+	scanf ("%d", &chieucao);
+	printf("Nhap vao day can nang : ");
+	scanf ("%d", &cannang);
+	printf("Nhap vao day mau toc : ");
+	scanf ("%s", &mautoc);
+	printf("Nhap vao day so do 3 vong : ");
+	scanf ("%d", &sdbv);
+}
+
+
+/** Ham check **/
+int check (int chieucao, int cannang, int  mautoc, int sdbv)
+{
+	if (sdbv == 905890)
+	{
+		printf(" Lam nguoi yeu anh nhe  %c or %c " , y or n );
+		scanf ("%c", &y);
+		scanf ("%c", &n );
+		if ( ans == n )
+		{
+			int thattinh (int n);
+			
+		}
+		if (ans == y)
+		{
+			 yethetminh(int y);
+		}
+	}
+	else{
+		printf("Khong phai gout anh roi ");
+	}
+}
+
+/**Ham that tinh **/
+int thattinh ( int n)
+{
+
+printf("Tai sao ta sinh ra lai khong danh cho nhau .... Thooi anh dua dayyy hihii ngokkk ");
+}
+
+
+/**Ham yeu het minh */
+{
+int yethetminh (int y)
+printf("Anh se danh ca quang doi con lai cham soc cho em ");
+}
